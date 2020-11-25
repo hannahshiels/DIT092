157105aff3cf64ae5f25f3472093f98a13fc60a4
shiels <shiels@student.chalmers.se>
2020-11-25 20:24:33 +0100

    Update User.java with list, getters,checks if registered

--- src/User.java
+++ src/User.java
@@ -1,26 +1,46 @@
+import java.util.ArrayList;
+
 public class User {
 
-    private String username;
-    private String name;
+    private String email;
     private String password;
+    static ArrayList<User> users = new ArrayList<>();
 
-    User(String username, String name, String password){
-        this.username = username;
-        this.name = name;
+    User(String username, String password){
+        this.email = username;
         this.password = password;
     }
 
-    public boolean checkUsername(String inputUsername) {
-        return this.username.equals(inputUsername);
+    public void addUser(User newUser){
+        users.add(newUser);
     }
 
-    public boolean checkPassword(String inputPassword) {
-        return this.password.equals(inputPassword);
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+    
+    public void listUsers(){
+        for(int i = 0; i < users.size(); i++){
+            System.out.println(users.get(i).toString());
+        }
+    }
+
+    public boolean isRegistered(String username, String password){
+        for(int i = 0; i < users.size(); i++){
+            if(users.get(i).getEmail().equalsIgnoreCase(username) && users.get(i).getPassword().equals(password)){
+                return true;
+            }
+        }
+        return false;
     }
 
 
     @Override
     public String toString() {
-        return "username: " + this.username;
+        return "email: " + this.email;
     }
 }
