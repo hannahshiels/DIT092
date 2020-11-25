fc5a9fec32c06334783d5f2af78473790e21084b
shiels <shiels@student.chalmers.se>
2020-11-25 20:27:57 +0100

    Update Project.java

--- src/Project.java
+++ src/Project.java
@@ -1,16 +1,25 @@
+import java.util.ArrayList;
+
 public class Project {
 
     private String projectName;
     private String projectDescription;
+    static ArrayList<Project> projects = new ArrayList<>();
 
     Project(String projectName, String projectDescription){
         this.projectName = projectName;
         this.projectDescription = projectDescription;
     }
 
+    public void listProjects(){
+        for(int i = 0; i < projects.size(); i++){
+            System.out.println(projects.get(i).toString());
+        }
+    }
+
     @Override
     public String toString() {
-        return "project name: " + this.projectName + "\n project description: " + this.projectDescription ;
+        return "project name: " + this.projectName + "\n project description: " + this.projectDescription;
     }
 
 }
