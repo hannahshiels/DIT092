065b75adf90d18ba9ac2db1dae17ac577833899f
shiels <shiels@student.chalmers.se>
2020-11-28 20:25:00 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class Main{
+public class Main extends Application{
 
     public static void main(String[] args){
-        LoginInterface.main(args);
+        launch(args);
     }
 
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        GridPane gui = new GridPane();
+        Scene scene = new Scene(gui, 900, 700);
+        InterfaceController.showMainMenu(primaryStage,scene);
+    }
 
 }
