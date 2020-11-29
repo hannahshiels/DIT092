ded8b7c14bbd62713e921052b98c9337e9740dc8
shiels <shiels@student.chalmers.se>
2020-11-29 15:35:14 +0100

    Updated LoginInterface.java with links to other screens

--- src/LoginInterface.java
+++ src/LoginInterface.java
@@ -4,6 +4,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -46,23 +47,50 @@ public class LoginInterface {
 
         //User login-form
         //User email
+
+        Hyperlink menuLink = new Hyperlink("Back to Main Menu");
+        GridPane.setConstraints(menuLink, 0, 0);
+        grid.getChildren().add(menuLink);
+
+        menuLink.setOnAction(new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                InterfaceController.switchToMainMenu(getStage(), getScene());
+            }
+        });
+
+
+
         final TextField userEmail = new TextField();
         userEmail.setPromptText("Enter user email");
         userEmail.setPrefColumnCount(10);
-        GridPane.setConstraints(userEmail, 0, 0);
+        GridPane.setConstraints(userEmail, 0, 1);
         grid.getChildren().add(userEmail);
 
         //User password
         final PasswordField userPassword = new PasswordField();
         userPassword.setPromptText("Enter user password");
         userPassword.setPrefColumnCount(10);
-        GridPane.setConstraints(userPassword, 0, 1);
+        GridPane.setConstraints(userPassword, 0, 2);
         grid.getChildren().add(userPassword);
 
         //submission button
         Button submit = new Button ("Login");
         GridPane.setConstraints(submit, 0, 3);
         grid.getChildren().add(submit);
+
+
+        Hyperlink createLink = new Hyperlink("Create an account instead");
+        GridPane.setConstraints(createLink, 0, 4);
+        grid.getChildren().add(createLink);
+
+        createLink.setOnAction(new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                InterfaceController.switchToRegister(getStage(), getScene());
+            }
+        });
+
 
         return grid;
     }
