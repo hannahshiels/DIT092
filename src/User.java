e00dcd896cd95f03ce4fae57e0f7f9dffeddac5d
shiels <shiels@student.chalmers.se>
2020-11-29 15:32:48 +0100

    Updated User.java with Anis' code

--- src/User.java
+++ src/User.java
@@ -1,28 +1,63 @@
 import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class User {
 
     private String email;
+    private String firstName;
+    private String lastName;
     private String password;
+    private LocalDate creationDate;
     static ArrayList<User> users = new ArrayList<>();
 
-    User(String email, String password){
+    User(String email, String firstName, String lastName, String password){
         this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.password = password;
+        this.creationDate = CurrentDate.getCurrentDate();
     }
 
     public void addUser(User newUser){
         users.add(newUser);
     }
 
+
+
+
     public String getEmail(){
         return this.email;
     }
 
+    public String getFirstName(){
+        return this.email;
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+        public String getLastName(){
+        return this.password;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
     public String getPassword(){
         return this.password;
     }
-    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+
+
     public void listUsers(){
         for(int i = 0; i < users.size(); i++){
             System.out.println(users.get(i).toString());
@@ -41,6 +76,6 @@ public class User {
 
     @Override
     public String toString() {
-        return "email: " + this.email;
+        return "User: \n " + "email: " + this.email + " \n first name: " + this.firstName + "\n last name: " + this.lastName + " \n account created: " + this.creationDate;
     }
 }
