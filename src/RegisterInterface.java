647159125eaa71bf5a1efac2fa493c1d768b1148
shiels <shiels@student.chalmers.se>
2020-11-29 15:38:02 +0100

    Updated RegisterInterface.java with links to other screens

--- src/RegisterInterface.java
+++ src/RegisterInterface.java
@@ -3,10 +3,7 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -32,6 +29,14 @@ public class RegisterInterface{
         return this.title;
     }
 
+    public  Stage getStage(){
+        return this.stage;
+    }
+    public Scene getScene(){
+        return this.scene;
+    }
+
+
 
     private GridPane showGUI(){
         GridPane grid = new GridPane();
@@ -39,25 +44,76 @@ public class RegisterInterface{
         grid.setVgap(5);
         grid.setHgap(5);
 
+
+        Hyperlink menuLink = new Hyperlink("Back to Main Menu");
+
+        menuLink.setOnAction(new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                InterfaceController.switchToMainMenu(getStage(), getScene());
+            }
+        });
+
         Label label = new Label("Create an account");
-        GridPane.setConstraints(label,0,0);
-        grid.getChildren().add(label);
+
+        final TextField userFirstName = new TextField();
+        userFirstName.setPromptText("Enter first name");
+
 
         TextField userEmail = new TextField();
-        userEmail.setPromptText("Enter user email");
+        userEmail.setPromptText("Enter email");
 
-        GridPane.setConstraints(userEmail, 0, 1);
-        grid.getChildren().add(userEmail);
+        final TextField userLastName = new TextField();
+        userLastName.setPromptText("Enter last name");
+
+
 
         PasswordField userPassword = new PasswordField();
-        userPassword.setPromptText("Enter your password");
+        userPassword.setPromptText("Enter password");
 
-        GridPane.setConstraints(userPassword, 0, 2);
-        grid.getChildren().add(userPassword);
+
+        final PasswordField userPasswordConfirm = new PasswordField();
+        userPasswordConfirm.setPromptText("Confirm password");
+
+
 
         Button createBtn = new Button ("Create account");
-        GridPane.setConstraints(createBtn, 0, 3);
+
+        Hyperlink loginLink = new Hyperlink("Log In instead");
+
+
+        loginLink.setOnAction(new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                InterfaceController.switchToLogin(getStage(), getScene());
+            }
+        });
+
+
+        GridPane.setConstraints(menuLink, 0, 0);
+        grid.getChildren().add(menuLink);
+        GridPane.setConstraints(label,0,1);
+        grid.getChildren().add(label);
+
+        GridPane.setConstraints(userFirstName, 0, 2);
+        grid.getChildren().add(userFirstName);
+        GridPane.setConstraints(userLastName, 0, 3);
+        grid.getChildren().add(userLastName);
+        GridPane.setConstraints(userEmail, 0, 4);
+        grid.getChildren().add(userEmail);
+        GridPane.setConstraints(userPassword, 0, 5);
+        grid.getChildren().add(userPassword);
+        GridPane.setConstraints(userPasswordConfirm, 0, 6);
+        grid.getChildren().add(userPasswordConfirm);
+
+        GridPane.setConstraints(createBtn, 0, 7);
         grid.getChildren().add(createBtn);
+        grid.getChildren().add(loginLink);
+        GridPane.setConstraints(loginLink, 0, 8);
+
+
+
+
          return grid;
 
     }
