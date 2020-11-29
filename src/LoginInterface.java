507885c8d616cfed3c0f1b2df531b071e06322c6
shiels <shiels@student.chalmers.se>
2020-11-29 18:06:47 +0100

    Update LoginInterface.java

--- src/LoginInterface.java
+++ src/LoginInterface.java
@@ -3,10 +3,7 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -74,14 +71,38 @@ public class LoginInterface {
         GridPane.setConstraints(userPassword, 0, 2);
         grid.getChildren().add(userPassword);
 
+        // debug label
+        Label debug = new Label();
+        grid.getChildren().add(debug);
+        GridPane.setConstraints(debug, 0, 4);
+
+
         //submission button
         Button submit = new Button ("Login");
         GridPane.setConstraints(submit, 0, 3);
         grid.getChildren().add(submit);
+        submit.setOnAction(new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                debug.setText("");
+                String email = userEmail.getText();
+                String password = userPassword.getText();
+                if(email.length() == 0 || password.length() == 0){
+                    debug.setText("Enter all fields");
+                } else if(User.isRegistered(email,password)){
+                    InterfaceController.switchToUserMenu(getStage(), getScene());
+                } else{
+                    debug.setText("Account not found.");
+                }
+
+            }
+        });
+
+
 
 
         Hyperlink createLink = new Hyperlink("Create an account instead");
-        GridPane.setConstraints(createLink, 0, 4);
+        GridPane.setConstraints(createLink, 0, 5);
         grid.getChildren().add(createLink);
 
         createLink.setOnAction(new EventHandler() {
