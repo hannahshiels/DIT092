7e6d7bcc0c9cfead12018865bfc7ead38840031a
shiels <shiels@student.chalmers.se>
2020-11-30 09:55:10 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,28 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        testingInit();
+
         GridPane gui = new GridPane();
         Scene scene = new Scene(gui, 900, 700);
         InterfaceController.showMainMenu(primaryStage,scene);
     }
+
+
+
+    public void testingInit(){
+
+        User user1 = new User("email@email.com", "firstName", "lastName", "password");
+        User.addUser(user1);
+        Project project1 = new Project("Project name 1", "Project Description 1", user1);
+        Project.addProject(project1);
+        Project project2 = new Project("Project name 2", "Project Description 2", user1);
+        Project.addProject(project2);
+        Project project3 = new Project("Project name 3", "Project Description 3", user1);
+        Project.addProject(project3);
+        User.listUsers();
+
+    }
+
 
 }
