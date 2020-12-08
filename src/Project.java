4d80d5d2dfb8a2593428f76fe26196f69796db53
shiels <shiels@student.chalmers.se>
2020-12-08 16:37:58 +0100

    Move methods to project library

--- src/Project.java
+++ src/Project.java
@@ -6,11 +6,11 @@ public class Project {
     private String projectDescription;
     private User user;
     private String projectOwner;
-    static ArrayList<Project> projects = new ArrayList<>();
 
-    Project(String projectName, String projectDescription, User user){
+    public Project(String projectName, String projectDescription, User user){
         this.projectName = projectName;
         this.projectDescription = projectDescription;
+        this.user = user;
         this.projectOwner = user.getEmail();
     }
 
@@ -24,41 +24,6 @@ public class Project {
     public String getProjectOwner(){
         return this.projectOwner;
     }
-
-
-
-
-    public static void addProject(Project newProject){
-        projects.add(newProject);
-    }
-
-    public void listAllProjects(){
-        for(int i = 0; i < projects.size(); i++){
-            System.out.println(projects.get(i).toString());
-        }
-    }
-
-    public static ArrayList<Project> getAllUserProjects(User user){
-        ArrayList<Project> userProjects = new ArrayList<>();
-        int count = 0;
-        for (int i = 0; i < projects.size(); i++){
-            if(projects.get(i).getProjectOwner().equals(user.getEmail())){
-                userProjects.add(count,projects.get(i));
-                count++;
-            }
-        }
-        return userProjects;
-    }
-
-    public static void listAllUserProjects(User user){
-        for(int i = 0; i < projects.size(); i++){
-            if(projects.get(i).getProjectOwner().equals(user.getEmail())){
-                System.out.println("Project name: " + projects.get(i).getProjectName());
-                System.out.println("Project description: " + projects.get(i).getProjectDescription());
-            }
-        }
-    }
-
 
 
     @Override
