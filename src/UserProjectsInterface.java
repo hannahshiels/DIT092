2121f2242207546b6f68928fa5e2c9d46fdad72c
shiels <shiels@student.chalmers.se>
2020-12-08 16:44:31 +0100

    Update UserProjectsInterface.java

--- src/UserProjectsInterface.java
+++ src/UserProjectsInterface.java
@@ -1,59 +1,74 @@
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 import java.util.ArrayList;
 
 public class UserProjectsInterface extends Interface {
 
     private User currentUser;
-    static  ArrayList<Project> userProjects = new ArrayList<>();
+    private ProjectLibrary projectLibrary;
+    ArrayList<Project> userProjects = new ArrayList<>();
 
-    UserProjectsInterface(User currentUser,Stage stage, Scene scene){
-        super(stage, scene);
+    public UserProjectsInterface(User currentUser){
+        super();
         super.setTitle("Miss Management | Current Projects");
         this.currentUser = currentUser;
-        this.userProjects = Project.getAllUserProjects(currentUser);
+        this.projectLibrary = new ProjectLibrary();
+        this.userProjects = projectLibrary.getAllUserProjects(currentUser);
     }
 
 
-    public User getCurrentUser() {
-        return currentUser;
+    public AnchorPane showGUI(){
+        AnchorPane root = new AnchorPane();
+
+        GridPane titlePane = getTitlePane("Miss Management | Current Projects");
+        GridPane userProjectsPane = getUserProjectsPane();
+        root.getChildren().add(titlePane);
+        root.getChildren().add(userProjectsPane);
+
+        AnchorPane.setTopAnchor(titlePane,0.0);
+        AnchorPane.setTopAnchor(userProjectsPane,120.0);
+
+        return root;
     }
 
 
-    public GridPane showGUI(){
+    private GridPane getUserProjectsPane(){
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(20, 20, 20, 20));
         grid.setVgap(10);
         grid.setHgap(5);
 
-        Hyperlink backToUserMenu = new Hyperlink("Back to User Menu");
-        backToUserMenu.setOnAction(new EventHandler() {
-            @Override
-            public void handle(Event event) {
-                InterfaceController.switchToUserMenu(getCurrentUser(),getStage(), getScene());
-            }
-        });
+
         GridPane.setConstraints(backToUserMenu, 0, 0);
         grid.getChildren().add(backToUserMenu);
-       int count = 2;
 
-       for(int i = 0; i < userProjects.size(); i++){
-            Button project = new Button(userProjects.get(i).getProjectName());
-            GridPane.setConstraints(project, 0, count);
-            grid.getChildren().add(project);
-            count++;
-        }
-
-
-
+        showAllProjects(1, grid);
+        grid.getStyleClass().add("grid");
 
         return grid;
+    }
+
+
+    private void showAllProjects(int startNum, GridPane grid){
+        // always returns null
+        // need to fix
+        if(userProjects == null){
+            System.out.println("no projects yet");
+        } else {
+            for(int i = 0; i < userProjects.size(); i++){
+                Button project = new Button(userProjects.get(i).getProjectName());
+                GridPane.setConstraints(project, 0, startNum);
+                grid.getChildren().add(project);
+                startNum++;
+            }
+        }
+
     }
 
 }
