26d682f863eeb40961ef7b07dd9905e1b4eb7c61
shiels <shiels@student.chalmers.se>
2020-12-10 18:39:45 +0100

    created new user sysadmin

---
+++ src/SysAdmin.java
@@ -0,0 +1,8 @@
+package users;
+
+public class SysAdmin extends User {
+
+    public SysAdmin(String email, String firstName, String lastName, String password) {
+        super(email, firstName, lastName, password);
+    }
+}
