e6a8541d07df9263133fc26229f98e4afc4c2138
shiels <shiels@student.chalmers.se>
2020-12-11 15:52:02 +0100

    Upload New File

---
+++ src/ManageTaskInterface.java
@@ -0,0 +1,73 @@
+package interfaces;
+
+import javafx.geometry.Insets;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class ManageTaskInterface extends Interface{
+    public ManageTaskInterface(){
+        super();
+        super.setTitle("Miss Management | Manage Task");
+
+    }
+
+    public AnchorPane showGUI(){
+        AnchorPane root = new AnchorPane();
+        GridPane titlePane = getTitlePane("Miss Management | Manage Project");
+        GridPane manageTaskNav = getManageTaskNav();
+        GridPane manageTaskMenu = getManageTaskMenu();
+
+        root.getChildren().add(titlePane);
+        root.getChildren().add(manageTaskNav);
+        root.getChildren().add(manageTaskMenu);
+
+        AnchorPane.setTopAnchor(titlePane,0.0);
+        AnchorPane.setTopAnchor(manageTaskNav,120.0);
+        AnchorPane.setTopAnchor(manageTaskMenu,240.0);
+
+
+
+        return root;
+
+    }
+
+
+
+    public GridPane getManageTaskNav(){
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(20, 20, 20, 20));
+        grid.setVgap(10);
+        grid.setHgap(5);
+
+        GridPane.setConstraints(backToCurrentTasks, 0, 0);
+        grid.getChildren().add(backToCurrentTasks);
+
+        GridPane.setConstraints(taskNameLabel, 0, 1);
+        grid.getChildren().add(taskNameLabel);
+
+        GridPane.setConstraints(taskDescLabel, 0, 2);
+        grid.getChildren().add(taskDescLabel);
+
+        grid.getStyleClass().add("grid");
+
+        return grid;
+    }
+
+    public GridPane getManageTaskMenu(){
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(20, 20, 20, 20));
+        grid.setVgap(10);
+        grid.setHgap(5);
+        taskProgress.getItems().addAll("Not started", "In progress", "Done");
+        GridPane.setConstraints(taskProgress, 0, 0);
+        grid.getChildren().add(taskProgress);
+
+        grid.getStyleClass().add("grid");
+
+        return grid;
+    }
+
+
+
+
+}
