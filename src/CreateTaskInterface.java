94e909b773c57dbc522edc96702ea8738f873298
shiels <shiels@student.chalmers.se>
2020-12-13 18:31:36 +0100

    Update CreateTaskInterface.java

--- src/CreateTaskInterface.java
+++ src/CreateTaskInterface.java
@@ -1,7 +1,6 @@
-import javafx.geometry.HPos;
+package interfaces;
+
 import javafx.geometry.Insets;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
