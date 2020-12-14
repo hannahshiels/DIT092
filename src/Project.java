0d95dbc42ecdf1deba4ea3e6c35e1331aaaa1285
shiels <shiels@student.chalmers.se>
2020-12-14 13:06:02 +0100

    moved product owner to role instead

--- src/Project.java
+++ src/Project.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
+
+
 public class Project {
 
     private String projectName;
     private String projectDescription;
-    private String projectOwner;
     private String ID;
+
 
     public Project(String projectName, String projectDescription, User user){
         this.projectName = projectName;
         this.projectDescription = projectDescription;
-        this.projectOwner = user.getEmail();
         this.ID = RandomID.getRandomID();
     }
 
@@ -23,14 +25,11 @@ public class Project {
         return this.projectDescription;
     }
 
-    public String getProjectOwner(){
-        return this.projectOwner;
-    }
 
 
     @Override
     public String toString() {
-        return "Project: " +  "\n id: " + this.ID +  "\n project owner: " + this.projectOwner +  "\n project name: " + this.projectName + "\n project description: " + this.projectDescription;
+        return "Project: " +  "\n id: " + this.ID +  "\n project name: " + this.projectName + "\n project description: " + this.projectDescription;
     }
 
 }
