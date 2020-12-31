7c7fb3f0b274a5c1ed729ac9c30e17f4b9ed66ae
shiels <shiels@student.chalmers.se>
2020-12-31 20:23:46 +0100

    Update RoleLibrary.java

--- src/RoleLibrary.java
+++ src/RoleLibrary.java
@@ -35,6 +35,29 @@ public class RoleLibrary {
     }
 
 
+    public int getNumberOfUsersInProject(String projectID){
+        int totalUsers = 0;
+        for(int i = 0; i < roleList.size(); i++){
+            Role role = roleList.get(i);
+            if(role.getProjectID().equals(projectID)){
+                totalUsers++;
+            }
+        }
+        return totalUsers;
+    }
+
+
+
+    public ArrayList<User> getAllProjectUsers(String ID){
+        ArrayList<User> allProjectUsers = new ArrayList<>();
+        for(Role role: roleList){
+            if(role.getProjectID().equals(ID)){
+                allProjectUsers.add(role.getUser());
+            }
+        } 
+        return allProjectUsers;
+    }
+
     public ArrayList<Role> getRoleListInProject(String projectID) {
         ArrayList<Role> rolesInProject = new ArrayList<>();
         for (Role role : roleList) {
@@ -65,6 +88,16 @@ public class RoleLibrary {
         }
         return false;
 
+    }
+
+    public Role getUserRoleInProject(String projectID, User user){
+        for(int i = 0; i < roleList.size(); i++){
+            Role currentRole = roleList.get(i);
+            if(currentRole.getProjectID().equals(projectID) && currentRole.getUser() == user){
+                return currentRole;
+            }
+        }
+        return null;
     }
 }
 
