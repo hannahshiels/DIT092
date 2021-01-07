8e3122b19b8796f825bc214d540fad7ca5bd18d8
shiels <shiels@student.chalmers.se>
2021-01-07 19:48:46 +0100

    add class to store meeting data

---
+++ src/projects/Meeting.java
@@ -0,0 +1,42 @@
+package projects;
+
+import users.User;
+
+import java.time.LocalDate;
+
+public class Meeting {
+    private String projectID;
+    private User meetingCreator;
+    private String location;
+    private LocalDate meetingDate;
+
+    public Meeting(String projectID, User meetingCreator, String location, LocalDate meetingDate){
+        this.projectID = projectID;
+        this.meetingCreator = meetingCreator;
+        this.location = location;
+        this.meetingDate = meetingDate;
+    }
+
+
+
+    public String getProjectID(){
+        return this.projectID;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public LocalDate getMeetingDate(){
+        return this.meetingDate;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "Meeting: " +  "\n project id: " + this.projectID +  "\n meeting creator: " + this.meetingCreator + "\n location: " + this.location + "\n meeting date: " + meetingDate;
+    }
+}
