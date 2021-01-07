f562a86ce5151f0c8664ff2b5ceb169f67ef66dd
shiels <shiels@student.chalmers.se>
2021-01-08 00:44:22 +0100

    Update MeetingLibrary.java

--- src/projects/MeetingLibrary.java
+++ src/projects/MeetingLibrary.java
@@ -1,5 +1,8 @@
 package projects;
 
+import users.User;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class MeetingLibrary {
@@ -9,5 +12,28 @@ public class MeetingLibrary {
         meetingList.add(newMeeting);
     }
 
+    public ArrayList<Meeting> getUpcomingMeetings(String projectID){
+        ArrayList<Meeting> upcomingMeetings = new ArrayList<>();
+        LocalDate today = LocalDate.now();
+        for (int i = 0; i < meetingList.size(); i++){
+            Meeting meeting = meetingList.get(i);
+            if(meeting.getProjectID().equals(projectID) && meeting.getMeetingDate().compareTo(today) > 0){
+                upcomingMeetings.add(meeting);
+            }
+        }
+        return upcomingMeetings;
+    }
+
+    public ArrayList<Meeting> getPastMeetings(String projectID){
+        ArrayList<Meeting> pastMeetings = new ArrayList<>();
+        LocalDate today = LocalDate.now();
+        for (int i = 0; i < meetingList.size(); i++){
+            Meeting meeting = meetingList.get(i);
+            if(meeting.getProjectID().equals(projectID) && meeting.getMeetingDate().compareTo(today) < 0){
+                pastMeetings.add(meeting);
+            }
+        }
+        return pastMeetings;
+    }
 
 }
