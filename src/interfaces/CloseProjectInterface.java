006ec520cc2d022d259444dff1c8c18dc8f7bc21
shiels <shiels@student.chalmers.se>
2021-01-08 00:37:05 +0100

    Update CloseProjectInterface.java

--- src/interfaces/CloseProjectInterface.java
+++ src/interfaces/CloseProjectInterface.java
@@ -5,6 +5,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
 public class CloseProjectInterface extends Interface {
+
     public CloseProjectInterface(){
         super();
         super.setTitle("Miss Management | Close a project");
@@ -48,6 +49,13 @@ public class CloseProjectInterface extends Interface {
         grid.setPadding(new Insets(20, 20, 20, 20));
         grid.setVgap(10);
         grid.setHgap(5);
+
+        GridPane.setConstraints(projectsCb, 0, 0);
+        grid.getChildren().add(projectsCb);
+        projectsCb.getStyleClass().add("cb-lg");
+
+        GridPane.setConstraints(closeProjectBtn, 0, 1);
+        grid.getChildren().add(closeProjectBtn);
 
         grid.getStyleClass().add("grid");
         grid.getStyleClass().add("center");
